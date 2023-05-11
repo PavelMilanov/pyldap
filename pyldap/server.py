3f08595ba34e5bfdab1ad8aa96001eaf53618b1b
Pavel Milanov <milanov71@icloud.com>
2023-05-11 11:53:26 +0300

    fix bag for connecting bd

--- pyldap/server.py
+++ pyldap/server.py
@@ -5,7 +5,6 @@ from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 from db.redis import RedisConnector
 from environs import Env
 
-
 env = Env()
 env.read_env()
 
@@ -68,7 +67,18 @@ app.add_middleware(
 
 @app.on_event("startup")
 async def startup_event():
-    pass
+    USER = env('POSTGRES_USER')
+    PASSWORD = env('POSTGRES_PASSWORD')
+    DB = env('POSTGRES_DB')
+    HOST = env('POSTGRES_HOST')
+
+    register_tortoise(
+        app,
+        db_url=f'postgres://{USER}:{PASSWORD}@{HOST}:5432/{DB}',
+        modules={"models": ['db.postgres.models']},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
 
 @app.on_event("shutdown")
 async def shutdown_event():
