fdc42c70a14271e9e3eb035eaf0ae18b8de5400f
Pavel Milanov <milanov71@icloud.com>
2023-07-11 10:00:47 +0300

    full change Units component

--- web/src/components/Units.vue
+++ web/src/components/Units.vue
@@ -19,9 +19,9 @@ export default {
     },
     methods: {
         async getComputers(unit) {
-            this.unitId = unit
-            let data = await this.store.GetUnitComputersList(unit)
-            this.computers = data
+          this.unitId = unit 
+          let data = await this.store.GetUnitComputersList(unit)
+          this.computers = data
         }
 
     },
@@ -34,20 +34,35 @@ export default {
 <template>
     <div class="row">
       <div class="col-2 p-4">
-        <nav id="navbar-unit" class="h-100 flex-column align-items-stretch pe-4 border-end">
-          <nav class="nav nav-pills flex-column" v-for="(subtree, tree) in this.store.getUnits" :key="tree">
-            <a class="nav-link" :href="`#${tree}`" @click="getComputers(tree)">{{ tree }}</a>
-            <nav class="nav nav-pills flex-column" v-for="(item, index) in subtree" :key="index">
-              <a class="nav-link ms-3" :href="`#${tree}-${item}`" @click="getComputers(`${tree}-${item}`)">{{ item }}</a>
-            </nav>
-          </nav>
-        </nav>
+        <ol>
+          <li v-for="(subtree, tree) in this.store.getUnits" :key="tree">
+            <a class="tree-link" :href="`#${tree}`" @click="getComputers(tree)">{{ tree }}</a>
+            <ol>
+              <li v-for="(item, index) in subtree" :key="index">
+                <a class="tree-link" :href="`#${tree}-${item}`" @click="getComputers(`${tree}-${item}`)">{{ item }}</a>
+              </li>
+            </ol>
+          </li>
+
+        </ol>
       </div>
-      <div class="col-8 p-4">
-        <div data-bs-spy="scroll" data-bs-target="#navbar-unit" data-bs-smooth-scroll="true" class="scrollspy-example-2" tabindex="0">
-          <div :id="`${unitId}`">
-            <p>Пользователи: {{ computers.join(', ') }}</p>
-          </div>
+      <div class="col-4 p-4">
+        <div class="table-container">
+          <table class="table table-bordered">
+            <caption class="caption-top">{{ this.unitId }}</caption>
+            <thead>
+              <tr>
+                <th scope="col">№</th>
+                <th scope="col">Пользователи</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in this.computers" :key="index">
+                <th scope="row">{{ index+1 }}</th>
+                <td>{{ item }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
@@ -55,4 +70,13 @@ export default {
 
 <style lang="less">
 
+.tree-link {
+  text-decoration: none;
+  color: rgb(41, 40, 40);
+}
+
+.table-container {
+  overflow: auto;
+  height: 65rem;
+}
 </style>
