94646a81e9a87871d76da42a3b22d2d6fa4ed823
Pavel Milanov <milanov71@icloud.com>
2023-07-26 17:39:30 +0300

    config backend server for netclient data

---
+++ backend/backend/migrations/models/0_20230726165525_init.py
@@ -0,0 +1,31 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE TABLE IF NOT EXISTS "acts" (
+    "id" UUID NOT NULL  PRIMARY KEY,
+    "customer" TEXT NOT NULL,
+    "file_name" TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "netclients" (
+    "system" VARCHAR(15) NOT NULL  PRIMARY KEY,
+    "network" TEXT NOT NULL,
+    "time" TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "static_ip" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "ip" VARCHAR(15) NOT NULL UNIQUE,
+    "description" TEXT
+);
+CREATE TABLE IF NOT EXISTS "aerich" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "version" VARCHAR(255) NOT NULL,
+    "app" VARCHAR(100) NOT NULL,
+    "content" JSONB NOT NULL
+);"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        """
